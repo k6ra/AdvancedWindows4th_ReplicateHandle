d6771a0b681639e56bfe5374d7e0dae6dde5441a
k6ra <m98213@gmail.com>
2019-03-23 17:40:52 +0900

    Create

---
+++ ReplicateHandle/ReplicateHandle.cpp
@@ -0,0 +1,32 @@
+﻿#include <windows.h>
+
+int main()
+{
+	HANDLE hFileMapRW = CreateFileMapping(
+		INVALID_HANDLE_VALUE,
+		NULL,
+		PAGE_READWRITE,
+		0,
+		10240,
+		NULL);
+
+	HANDLE hFileMapRO;
+	DuplicateHandle(
+		GetCurrentProcess(),
+		hFileMapRW,
+		GetCurrentProcess(),
+		&hFileMapRO,
+		FILE_MAP_READ,
+		FALSE,
+		0);
+	
+	/*
+	...
+	*/
+	CloseHandle(hFileMapRO);
+	
+	/*
+	...
+	*/
+	CloseHandle(hFileMapRW);
+}
